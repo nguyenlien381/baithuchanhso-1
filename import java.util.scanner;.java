e702ba0545228337a2cb515e8751dcba80f28a27
nguyenlien381 <97586322+nguyenlien381@users.noreply.github.com>
2022-01-21 09:56:33 +0700

    Add files via upload

---
+++ import java.util.scanner;.java
@@ -0,0 +1,36 @@
+import java.util.scanner;
+public calss app2{
+    public private static void main(string[] args)throws exception {
+        // khai bao bien
+         double hesoA1, hesoB1, hesoC1 ,hesoA2, hesoB2, hesoC2,D, Dx, Dy,x,y;
+        //can thong bao cho nguoi dung nhap vao he so
+        system.out.println("nhap he so A1");
+        scanner SC = new scanner(system.in);
+        hesoA1 = SC.nextdouble();
+        system.out.println("nhap he so B1");
+        scanner SC = new scanner(system.in);
+        hesoB1 = SC.nextdouble();
+        System.out.println("nhap he so C1");
+        hesoC1= SC.nextdouble();
+        System.out.println("nhap he so A2");
+        scanner SC = new scanner(system.in);
+        hesoA2 = SC.nextdouble();
+       System.out.println"nhap he so B2");
+        scanner SC = new scanner(system.in);
+        hesoB2 = SC.nextdouble();
+        System.out.println"nhap he so C2);
+        hesoC2= SC.nextdouble();
+        scanner.nextdouble();
+        system.out.println"(D = A1*B2-A2*B1
+        Dx = C1*B2-C2*B1
+        Dy =A1*C2-A2*C1);
+        if(D =0)
+        system.out.println("he phuong trinh co vo so nghiem");
+        eles
+        system.out.println("he phuong trinh vo nghiem");
+    }
+        eles{
+            x = Dx/D;
+            Y = Dy/D;
+            System.out.println("he phuong trinh co nghiem")
+        }
