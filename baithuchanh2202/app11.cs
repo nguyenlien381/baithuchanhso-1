9df5a3d0c63e4fe6e67d344b690583517dadfde0
nguyenlien381 <97586322+nguyenlien381@users.noreply.github.com>
2022-02-23 16:07:56 +0700

    Add files via upload

---
+++ baithuchanh2202/app11.cs
@@ -0,0 +1,19 @@
+public class App11 {
+    public static void main(String[] args) {
+        int tong = 200000;
+        int a = 50000;
+        int b = 20000;
+        int c = 10000;
+
+        for (int i = 0; i <= tong / a; i++) {
+            for (int j = 0; (j*b + i*a) <= tong; j++) {
+                for (int k = 0; (k*c + j*b + i*a) <= tong; k++) {
+                    if ((k*c + j*b + i*a) == tong){
+                        System.out.println(i + " to 50000, " + j + " to 20000, " + k + " to 10000");
+                    }
+                }
+            }
+        }
+    }
+}
+Viết cho Nguyễn Liên
