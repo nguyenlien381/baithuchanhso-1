b45a15f6ab701d0333e1fb8037dc554126cc2023
nguyenlien381 <97586322+nguyenlien381@users.noreply.github.com>
2022-03-02 14:07:39 +0700

    Add files via upload

---
+++ baithuchanh0203/app79.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+  public static void main(string[] args) {
+      int tong = 0;
+      for(int i=1; i<=n; i++) {
+          if(i%2==0) {
+              tong+=i;
+         }
+      }
+      System.out.println(" tong cac so chan cua mang la :"+tong);
+  }
