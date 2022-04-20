36f1eddca3bc5dbdbe155b0f2dcfa75f78396723
nguyenlien381 <97586322+nguyenlien381@users.noreply.github.com>
2022-04-20 16:21:50 +0700

    Add files via upload

---
+++ baithuchanh2004/public class TaxiCar implements itc {.java
@@ -0,0 +1,41 @@
+public class TaxiCar implements itc {
+    public void Move(){
+        System.out.println("Moving");
+    }
+    public void Stop(){
+        System.out.println("Stoped");
+    }
+    public void TurnRight(){
+        System.out.println("Turning right");
+    }
+    public void TurnLeft(){
+        System.out.println("Turning left");
+    }
+    public void Reverse(){
+        System.out.println("Reversed");
+    }
+}
+
+
+
+
+
+
+
+public class Tiger extends Animal {
+    public void Eat(){
+        System.out.println("Eating...!");
+    }
+    public void Sleep(){
+        System.out.println("Sleeping...!");
+    }
+    public void Walk(){
+        System.out.println("Walking...!");
+    }
+    public void Run(){
+        System.out.println("Running...!");
+    }
+    public void Roar(){
+        System.out.println("ROARRR!");
+    }
+}
